2063e728d63444fb4972712513450e7206e33568
Alia Quintero <aliaquintero@gmail.com>
2023-09-11 14:57:56 -0400

    remove dependencies

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,21 @@ import "./Hero.css";
 export default function Hero() {
   return (
     <header>
-      <h1>
-        Hey, I&apos;m <span>Alia</span>.
-      </h1>
-      <h2>
-        A <span>Front End</span> Web Developer.
-      </h2>
-      <p>
-        I&apos;ve always been passionate about creativity and
-        problem-solving, and I wanted to incorporate that into my career. I learned that I
-        had a penchant for front-end technologies.  My vision
-        for my career is to utilize these skills to support missions in climate
-        technology and web accessibility. Being a self-taught developer is my biggest strength, as I&apos;ve
-        facilitated my own learning outside of the traditional path and have
-        strengthened many core values in doing so. 
-      </p>
+        <h1>
+          Hey, I&apos;m <span>Alia</span>.
+        </h1>
+        <h2>
+          A <span>Front End</span> Web Developer.
+        </h2>
+        <p>
+          I&apos;ve always been passionate about creativity and problem-solving,
+          and I wanted to incorporate that into my career. I learned that I had
+          a penchant for front-end technologies. My vision for my career is to
+          utilize these skills to support missions in climate technology and web
+          accessibility. Being a self-taught developer is my biggest strength,
+          as I&apos;ve facilitated my own learning outside of the traditional
+          path and have strengthened many core values in doing so.
+        </p>
     </header>
   );
 }
