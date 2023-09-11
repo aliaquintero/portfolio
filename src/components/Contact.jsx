0ad559f0afb550383568494fc649379df3a7bc57
Alia Quintero <aliaquintero@gmail.com>
2023-09-11 18:30:44 -0400

    add react-hook-form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,52 @@
 import "./Contact.css";
+import { useForm } from "react-hook-form";
 
 export default function Contact() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Clicked!");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
+    alert("submitted");
   };
   return (
     <form
       action="POST"
       name="contact"
       data-netlify="true"
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <h3>Get In Touch</h3>
       <label htmlFor="name">Your Name</label>
-      <input type="text" id="name" />
+      <input type="text" id="name" {...register("name", { required: true })} />
+      {errors.name && <span>Your name is required</span>}
+
       <label htmlFor="email">Your Email</label>
-      <input type="email" id="email" />
+      <input
+        type="email"
+        id="email"
+        {...register("email", { required: true })}
+      />
+      {errors.email && errors.email.type === "pattern" && (
+        <span>Email is not valid</span>
+      )}
+
+      {errors.email && errors.email.type === "required" && (
+        <span>An email is required</span>
+      )}
+
       <label htmlFor="message">Your Message</label>
-      <textarea name="message" id="message" cols="28" rows="8"></textarea>
+      <textarea
+        name="message"
+        id="message"
+        cols="28"
+        rows="8"
+        {...register("message", { required: true })}
+      ></textarea>
+      {errors.message && <span>A message is required</span>}
+
       <button type="submit">Send</button>
     </form>
   );
