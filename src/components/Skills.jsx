bb4e61db5ee9d962d1a513dc1c8449948067f0d4
Alia Quintero <aliaquintero@gmail.com>
2023-09-11 14:40:59 -0400

    fix className

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,27 +6,27 @@ export default function Skills() {
       <h3>Skills</h3>
       <ul>
         <li>
-          <i class="fa-brands fa-html5" title="HTML"></i>
+          <i className="fa-brands fa-html5" title="HTML"></i>
           <p>HTML</p>
         </li>
         <li>
-          <i class="fa-brands fa-css3-alt" title="CSS"></i>
+          <i className="fa-brands fa-css3-alt" title="CSS"></i>
           <p>CSS</p>
         </li>
         <li>
-          <i class="fa-brands fa-square-js" title="JavaScript"></i>
+          <i className="fa-brands fa-square-js" title="JavaScript"></i>
           <p>JavaScript</p>
         </li>
         <li>
-          <i class="fa-brands fa-bootstrap" title="Bootstrap"></i>
+          <i className="fa-brands fa-bootstrap" title="Bootstrap"></i>
           <p>Bootstrap</p>
         </li>
         <li>
-          <i class="fa-brands fa-react" title="React"></i>
+          <i className="fa-brands fa-react" title="React"></i>
           <p>React</p>
         </li>
         <li>
-          <i class="fa-brands fa-github" title="GitHub"></i>
+          <i className="fa-brands fa-github" title="GitHub"></i>
           <p>GitHub</p>
         </li>
       </ul>
