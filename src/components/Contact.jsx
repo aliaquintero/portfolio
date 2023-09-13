4159b54c9aac3b0766aa7c60e9d4c24862e5092b
Alia Quintero <aliaquintero@gmail.com>
2023-09-12 20:09:18 -0400

    form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,21 +2,16 @@ import "./Contact.css";
 
 export default function Contact() {
   return (
-      <form
-        name="contact-form"
-        method="POST"
-        data-netlify="true"
-        onSubmit="submit"
-      >
-        <h3>Get In Touch</h3>
-        <input type="hidden" name="contact-form" value="contact-form" />
-        <label htmlFor="name">Your Name</label>
-        <input type="text" name="name" id="name" />
-        <label htmlFor="email">Your Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="message">Your Message</label>
-        <textarea name="message" id="message" cols="30" rows="10"></textarea>
-        <button type="submit">Send</button>
-      </form>
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
+      <h3>Get In Touch</h3>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" name="name" />
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" name="email" />
+      <label htmlFor="message">Message</label>
+      <textarea id="message" name="message" rows="6"></textarea>
+      <button type="submit">Send</button>
+    </form>
   );
 }
