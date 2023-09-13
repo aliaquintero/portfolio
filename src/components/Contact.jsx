40b955ee5be8c5d8d1c408aced422fb9830d5adb
Alia Quintero <aliaquintero@gmail.com>
2023-09-12 21:03:10 -0400

    add validation to form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,11 @@ export default function Contact() {
       <input type="hidden" name="form-name" value="contact" />
       <h3>Get In Touch</h3>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" name="name" />
+      <input type="text" id="name" name="name" required />
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" />
+      <input type="email" id="email" name="email" required />
       <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" rows="6"></textarea>
+      <textarea id="message" name="message" rows="6" required></textarea>
       <button type="submit">Send</button>
     </form>
   );
