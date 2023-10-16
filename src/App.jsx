4b78407538f4694e0919f278c57b342e8073f5d3
Alia Quintero <aliaquintero@gmail.com>
2023-10-16 16:50:50 -0400

    add react-scroll-motion

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,42 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  ZoomIn,
+} from "react-scroll-motion";
 import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Skills />
-      <Projects />
-      <Contact />
+    <ScrollContainer>
+      <ScrollPage class="scroll-container">
+        <Animator animation={batch(Fade(), ZoomIn())}>
+          <Header />
+        </Animator>
+      </ScrollPage>
+      <ScrollPage class="scroll-container">
+        <Animator animation={batch(FadeIn(), ZoomIn(2, 1))}>
+          <Skills />
+        </Animator>
+      </ScrollPage>
+      <ScrollPage class="scroll-container">
+        <Animator animation={batch(FadeIn(), ZoomIn(2, 1))}>
+          <Projects />
+        </Animator>
+      </ScrollPage>
+      <ScrollPage class="scroll-container">
+        <Animator animation={batch(FadeIn(), ZoomIn(2, 1))}>
+          <Contact />
+        </Animator>
+      </ScrollPage>
       <Footer />
-    </>
+    </ScrollContainer>
   );
 };
 
